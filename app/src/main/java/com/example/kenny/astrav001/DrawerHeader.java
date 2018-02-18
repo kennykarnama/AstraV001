1de9f08f9c510e57b023f2f41977d3111b77c327
kenny <kennykarnama@gmail.com>
2018-02-18 15:25:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kenny/astrav001/DrawerHeader.java
@@ -0,0 +1,33 @@
+package com.example.kenny.astrav001;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+/**
+ * Created by kenny on 2/18/18.
+ */
+@NonReusable
+@Layout(R.layout.drawer_header)
+
+public class DrawerHeader {
+
+    @View(R.id.profileImageView)
+    private ImageView profileImage;
+
+    @View(R.id.nameTxt)
+    private TextView nameTxt;
+
+    @View(R.id.emailTxt)
+    private TextView emailTxt;
+
+    @Resolve
+    private void onResolved() {
+        nameTxt.setText("Janishar Ali");
+        emailTxt.setText("janishar.ali@gmail.com");
+    }
+}
